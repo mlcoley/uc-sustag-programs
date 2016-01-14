af7f1ce84ff981e9c3df943d6ba9138362680681
mlcoley <mlcoley@ucdavis.edu>
2016-01-13 22:05:52 -0800

    Format contacts column
    Also, add email to contacts column, add campus and activities filters

--- js/main_tmp.js
+++ js/main_tmp.js
@@ -2,14 +2,20 @@ MyApp = {};
 MyApp.spreadsheetData = [];
 MyApp.keywords = [];
 MyApp.headerData = [
-    { "sTitle": "Program" }, { "sTitle": "Description" }, { "sTitle": "Website" }, { "sTitle": "Contacts" }, { "sTitle": "Email" }, {"sTitle": "Tags"}
+    { "sTitle": "Program" },
+    { "sTitle": "Description" },
+    { "sTitle": "Website" },
+    { "sTitle": "Contacts" },
+    {"sTitle": "Tags"},
+    {"sTitle": "Campus"},
+    {"sTitle": "Activities"}
 ];
-//filterIndexes is a map between names and the index in headerData (likely spreadsheetData too)
-MyApp.filterIndexes = { "tag": 5};
-MyApp.Tags = [];
+//filterIndexes is a map between names and the index in headerData (spreadsheetData too)
+MyApp.filterIndexes = { "tag": 4, "campus": 5, "activities": 6};
+MyApp.Tags = [], MyApp.Campus = [], MyApp.Activities = [];
 
 String.prototype.trunc = function (n) {
-    return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
+    return this.substr(0, n - 1) + (this.length > n ? "&hellip;" : "");
 };
 
 /* instructions for making a spreadsheet accessible
@@ -17,38 +23,52 @@ http://billing.chillidoghosting.com/knowledgebase/48/Publishing-Spreadsheets-to-
 */
 
 $(function () {
-    // var url = "https://spreadsheets.google.com/feeds/list/1GdRFYqi725g_ySoRRX_v7MTlQnusbCFST5smhZEzczc/1/public/values?alt=json-in-script&callback=?";
     var url = "https://spreadsheets.google.com/feeds/list/1Yrq7iNjFIUGv9N1DLATs_WlP-i9VUQYqb23lnUaQMJA/1/public/values?alt=json-in-script&callback=?";
     $.getJSON(url, {}, function (data) {
         $.each(data.feed.entry, function (key, val) {
             var prog = val.gsx$programs.$t;
-            var desc = val.gsx$description.$t;
+            // var desc = val.gsx$description.$t;
             var website = "<a target='_blank' href='" + val.gsx$website.$t + "'><i class='icon-globe'></i> Website</a>";
-            var contacts = val.gsx$contacts.$t;
-            var email = "<a href='mailto:" + val["gsx$email"].$t + "'><i class='icon-envelope'></i> Email</a>";
+            // var contacts = val.gsx$contacts.$t;
+            // var email = "<a href='mailto:" + val.gsx$email.$t + "'><i class='icon-envelope'></i> Email</a>";
             var tags = val.gsx$tags.$t;
+            var campus = val.gsx$campus.$t;
+            var activities = val.gsx$activities.$t;
 
-            MyApp.spreadsheetData.push(
-                [
-                    prog, GenerateDescription(val), website, contacts, email, tags
-                ]);
+            MyApp.spreadsheetData.push([
+                    prog,
+                    GenerateDescription(val),
+                    website,
+                    GenerateContact(val),
+                    tags,
+                    campus,
+                    activities
+                  ]);
 
-            $.each(tags.trim().replace(/^[\r\n]+|\.|[\r\n]+$/g, "").split(';'), function (key, val) {
-                val = val.trim(); //need to trim the semi-colon separated values after split
-
-                if ($.inArray(val, MyApp.Tags) === -1 && val.length !== 0) {
-                    MyApp.Tags.push(val);
-                }
-            });
+            parseList(tags, ";", "Tags");
+            parseList(campus, ";", "Campus");
+            parseList(activities, ";", "Activities");
         });
 
         MyApp.Tags.sort();
+        MyApp.Campus.sort();
+        MyApp.Activities.sort();
 
         createDataTable();
         addFilters();
         configurePopups();
     });
 })
+
+function parseList(str, sep, field) {
+    $.each(str.trim().replace(/^[\r\n]+|\.|[\r\n]+$/g, "").split(sep), function (key, val) {
+        val = val.trim(); //need to trim the semi-colon separated values after split
+
+        if ($.inArray(val, MyApp[field]) === -1 && val.length !== 0) {
+            MyApp[field].push(val);
+        }
+    });
+}
 
 // function hideUnavailableOrganizations(){
 //     var fileredData = MyApp.oTable._('tr', {"filter":"applied"});
@@ -81,13 +101,23 @@ function configurePopups(){
     });
 }
 
-
-
 function addFilters(){
     var $tags = $("#tags");
 
     $.each(MyApp.Tags, function (key, val) {
         $tags.append('<li><label><input type="checkbox" name="' + val + '"> ' + val + '</label></li>');
+    });
+
+    var $campus = $("#campus");
+
+    $.each(MyApp.Campus, function (key, val) {
+        $campus.append('<li><label><input type="checkbox" name="' + val + '"> ' + val + '</label></li>');
+    });
+
+    var $activities = $("#activities");
+
+    $.each(MyApp.Activities, function (key, val) {
+        $activities.append('<li><label><input type="checkbox" name="' + val + '"> ' + val + '</label></li>');
     });
 
     $(".filterrow").on("click", "ul.filterlist", function (e) {
@@ -137,22 +167,41 @@ function GenerateResearcherColumn(val /* entry value from spreadsheet */){
     return researcher;
 }
 
+function GenerateContact(val) {
+    var contacts = val.gsx$contacts.$t.trim().split(";");
+    console.log(contacts.length);
+    var str = "#name#<br /><span class='discreet'>#title#</span>";
+    var info = "";
+    var re = /([^\[]*)/g;
+    var re2 = /\[([^\]]*)\]/g;
+    for (k = 0; k < contacts.length; k++){
+        if (contacts[k]) {
+            var name = re.exec(contacts[k]);
+            if (name) {
+                info += name[1];
+            }
+
+            var title = re2.exec(contacts[k]);
+            if (title) {
+                info += "<br /><span class='discreet'>" + title[1] + "</span>";
+            } else {
+                console.log("no title " + contacts[k])
+            }
+            info += "<br />";
+        }
+    }
+    info += "<a href='mailto:" + val.gsx$email.$t + "'><i class='icon-envelope'></i></a>";
+    return info;
+}
+
 function GenerateDescription(val /* entry value from spreadsheet */){
     var desc_short = "<span style='font-size: 0.8em;'>" + val.gsx$description.$t.trunc(80) + "</span>";
-    var tmp = "Title";
+    var tmp = val.gsx$programs.$t;
+
     /*replace single-quotes with html code*/
     var desc = val.gsx$description.$t.replace(/'/g, "&#39;");
+
     var desc_full = "<a href='#' class='project-popover' data-toggle='popover' data-content='" + desc + "' data-original-title='" + tmp + "'>" + desc_short + "</a>";
-
-    // var project2title = "<span style='font-size: 0.8em;'>" + val.gsx$project2title.$t.trunc(20) + "</span>";
-    // var project2details = "Status: " + val.gsx$expectedcompletiondate_2.$t + (val.gsx$linktoprojectwebsite_2.$t ? "—" + val.gsx$linktoprojectwebsite_2.$t : '');
-    // var project2 = "<a href='#' class='project-popover' data-toggle='popover' data-content='" + project2details + "' data-original-title='" + val.gsx$project2title.$t + "'>" + project2title + "</a>";
-    //
-    // var project3title = "<span style='font-size: 0.8em;'>" + val.gsx$project3title.$t.trunc(20) + "</span>";
-    // var project3details = "Status: " + val.gsx$expectedcompletiondate_3.$t + (val.gsx$linktoprojectwebsite_3.$t ? "—" + val.gsx$linktoprojectwebsite_3.$t : '');
-    // var project3 = "<a href='#' class='project-popover' data-toggle='popover' data-content='" + project3details + "' data-original-title='" + val.gsx$project3title.$t + "'>" + project3title + "</a>";
-
-    // var projects = project1 + (val.gsx$project2title.$t ? project2 : '') + (val.gsx$project3title.$t ? project3 : '');
 
     return desc_full;
 }
@@ -207,7 +256,7 @@ function createDataTable() {
     MyApp.oTable = $("#spreadsheet").dataTable({
         "aoColumnDefs": [
             //{ "sType": "link-content", "aTargets": [ 0 ] },
-            { "bVisible": false, "aTargets": [ -1 ] } //hide the tags column (the last one)
+            { "bVisible": false, "aTargets": [ MyApp.filterIndexes["tag"] ] } //hide the tags column
         ],
         "iDisplayLength": 20,
         "bLengthChange": false,
