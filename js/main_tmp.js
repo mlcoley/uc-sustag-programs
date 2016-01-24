38127c94271eacc2b680ea5915238b33bb817f4e
mlcoley <mlcoley@ucdavis.edu>
2016-01-24 13:51:55 -0800

    Being fix of popover positioning
    Add js functions from ‘Hainesy’ (on SO) to compute position for popover
    as needed

--- js/main_tmp.js
+++ js/main_tmp.js
@@ -93,31 +93,6 @@ $(function () {
     });
 })
 /* ------------------------------------------------------------------------- */
-function generateIcon(str) {
-    return "<i class='ucicon-" + toFieldname(str) + "'></i>"
-}
-/* ------------------------------------------------------------------------- */
-function generateFilterBox() {
-    var template = '<div class="accordion-group">' +
-    '   <div class="accordion-heading">' +
-    '     <a class="accordion-toggle" data-toggle="collapse" href="#collapse$FIELD$">' +
-    '       $NAME$ <i class="icon-chevron-down pull-right"></i>' +
-    '     </a>' +
-    '   </div>' +
-    '   <div id="collapse$FIELD$" class="accordion-body collapse">' +
-    '       <div class="accordion-inner">' +
-    '           <ul id="$FIELD$" class="unstyled filterlist"></ul>' +
-    '       </div>' +
-    '   </div>' +
-    '</div>';
-    for (var k in filterColumns) {
-        var field = filterColumns[k];
-        var str = template.replace(/\$FIELD\$/g, toFieldname(field));
-        str = str.replace(/\$NAME\$/g, field);
-        document.getElementById("filterAccordion").innerHTML += str;
-    }
-}
-/* ------------------------------------------------------------------------- */
 function getIndex(x) {
     var out = null;
     for (var k in MyApp.headerData) {
@@ -154,12 +129,166 @@ function parseList(str, sep, field) {
             list: items
     }; //remove tailing line break(s)
 }
-/* ------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------
+NOTE: this following two sections are borrowed from Hainesy via stackoverflow
+http://stackoverflow.com/questions/10238089/how-can-you-ensure-twitter-bootstrap-popover-windows-are-visible
+   ------------------------------------------------------------------------- */
 function configurePopups(){
-    $("#spreadsheet").popover({
-        selector: '.researcher-popover, .project-popover',
-        trigger: 'hover'
+    $('[data-toggle="popover"]').each(function() {
+        var trigger = $(this);
+        trigger.popover({
+            animation: true,
+            delay: { show: 0, hide: 0 },
+            html: true,
+            trigger: 'hover',
+            placement: getPlacementFunction(trigger.attr("data-placement"), 200, 300)
+        });
     });
+}
+/* -------------------------------------------------------------------------
+NOTE: this still need work, esp. when the hovered element is exactly in the
+middle of the screen (i think)
+   ------------------------------------------------------------------------- */
+var getPlacementFunction = function (defaultPosition, width, height) {
+    return function (tip, element) {
+        var position, top, bottom, left, right;
+
+        var $element = $(element);
+        var boundTop = $(document).scrollTop(); /*NOTE: hardcode */
+        var boundLeft = $(document).scrollLeft();
+        var boundRight = boundLeft + $(window).width();
+        var boundBottom = boundTop + $(window).height();
+
+        console.log([boundTop, boundLeft, boundRight, boundBottom]);
+
+        var pos = $.extend({}, $element.offset(), {
+            width: element.offsetWidth,
+            height: element.offsetHeight
+        });
+
+        var isWithinBounds = function (elPos) {
+            return boundTop < elPos.top && boundLeft < elPos.left && boundRight > (elPos.left + width) && boundBottom > (elPos.top + height);
+        };
+
+        var testTop = function () {
+            if (top === false) return false;
+            top = isWithinBounds({
+                top: pos.top - height,
+                left: pos.left + pos.width / 2 - width / 2
+            });
+            return top ? "top" : false;
+        };
+
+        var testBottom = function () {
+            if (bottom === false) return false;
+            bottom = isWithinBounds({
+                top: pos.top + pos.height,
+                left: pos.left + pos.width / 2 - width / 2
+            });
+            return bottom ? "bottom" : false;
+        };
+
+        var testLeft = function () {
+            if (left === false) return false;
+            left = isWithinBounds({
+                top: pos.top + pos.height / 2 - height / 2,
+                left: pos.left - width
+            });
+            return left ? "left" : false;
+        };
+
+        var testRight = function () {
+            if (right === false) return false;
+            right = isWithinBounds({
+                top: pos.top + pos.height / 2 - height / 2,
+                left: pos.left + pos.width
+            });
+            return right ? "right" : false;
+        };
+
+        switch (defaultPosition) {
+            case "top":
+                if (position = testTop()) return position;
+            case "bottom":
+                if (position = testBottom()) return position;
+            case "left":
+                if (position = testLeft()) return position;
+            case "right":
+                if (position = testRight()) return position;
+            default:
+                if (position = testTop()) return position;
+                if (position = testBottom()) return position;
+                if (position = testLeft()) return position;
+                if (position = testRight()) return position;
+                return defaultPosition;
+        }
+    }
+};
+/* ------------------------------------------------------------------------- */
+function GenerateDescription(val /* entry value from spreadsheet */){
+    var desc_short = "<span style='font-size: 0.8em;'>" + val.gsx$description.$t.trunc(80) + "</span>";
+    var tmp = val.gsx$programs.$t;
+
+    /*replace single-quotes with html code, replace newline with <br>*/
+    var desc = val.gsx$description.$t.replace(/'/g, "&#39;").replace(/\n+/g, "<br>");
+
+    var desc_full = "<a href='#' class='project-popover' data-toggle='popover' data-content='" +
+        desc + "' data-html='true' data-original-title='" +
+        tmp + "'>" +
+        desc_short +
+        "</a>";
+
+    return desc_full;
+}
+/* ------------------------------------------------------------------------- */
+function generateIcon(str) {
+    return "<i class='ucicon-" + toFieldname(str) + "'></i>"
+}
+/* ------------------------------------------------------------------------- */
+function generateFilterBox() {
+    var template = '<div class="accordion-group">' +
+    '   <div class="accordion-heading">' +
+    '     <a class="accordion-toggle" data-toggle="collapse" href="#collapse$FIELD$">' +
+    '       $NAME$ <i class="icon-chevron-down pull-right"></i>' +
+    '     </a>' +
+    '   </div>' +
+    '   <div id="collapse$FIELD$" class="accordion-body collapse">' +
+    '       <div class="accordion-inner">' +
+    '           <ul id="$FIELD$" class="unstyled filterlist"></ul>' +
+    '       </div>' +
+    '   </div>' +
+    '</div>';
+    for (var k in filterColumns) {
+        var field = filterColumns[k];
+        var str = template.replace(/\$FIELD\$/g, toFieldname(field));
+        str = str.replace(/\$NAME\$/g, field);
+        document.getElementById("filterAccordion").innerHTML += str;
+    }
+}
+/* ------------------------------------------------------------------------- */
+function GenerateContact(val) {
+    var contacts = val.gsx$contacts.$t.trim().split(";");
+    var str = "#name#<br /><span class='discreet'>#title#</span>";
+    var info = "";
+    var re = /([^\[]*)/g;
+    var re2 = /\[([^\]]*)\]/g;
+    for (var k in contacts){
+        if (contacts[k]) {
+            var name = re.exec(contacts[k]);
+            if (name) {
+                info += name[1];
+            }
+
+            var title = re2.exec(contacts[k]);
+            if (title) {
+                info += "<br /><span class='discreet'>" + title[1] + "</span>";
+            }
+            info += "<br />";
+        }
+    }
+    info += "<a target='_blank' href='" + val.gsx$website.$t + "'><i class='icon-globe'></i> </a>";
+    info += "<a href='mailto:" + val.gsx$email.$t + "'><i class='icon-envelope'></i></a>";
+    return info;
 }
 /* ------------------------------------------------------------------------- */
 function addFilters(){
@@ -204,47 +333,6 @@ function addFilters(){
 
         $("ul.filterlist").click();
     });
-}
-/* ------------------------------------------------------------------------- */
-function GenerateContact(val) {
-    var contacts = val.gsx$contacts.$t.trim().split(";");
-    var str = "#name#<br /><span class='discreet'>#title#</span>";
-    var info = "";
-    var re = /([^\[]*)/g;
-    var re2 = /\[([^\]]*)\]/g;
-    for (var k in contacts){
-        if (contacts[k]) {
-            var name = re.exec(contacts[k]);
-            if (name) {
-                info += name[1];
-            }
-
-            var title = re2.exec(contacts[k]);
-            if (title) {
-                info += "<br /><span class='discreet'>" + title[1] + "</span>";
-            }
-            info += "<br />";
-        }
-    }
-    info += "<a target='_blank' href='" + val.gsx$website.$t + "'><i class='icon-globe'></i> </a>";
-    info += "<a href='mailto:" + val.gsx$email.$t + "'><i class='icon-envelope'></i></a>";
-    return info;
-}
-/* ------------------------------------------------------------------------- */
-function GenerateDescription(val /* entry value from spreadsheet */){
-    var desc_short = "<span style='font-size: 0.8em;'>" + val.gsx$description.$t.trunc(80) + "</span>";
-    var tmp = val.gsx$programs.$t;
-
-    /*replace single-quotes with html code, replace newline with <br>*/
-    var desc = val.gsx$description.$t.replace(/'/g, "&#39;").replace(/\n+/g, "<br>");
-
-    var desc_full = "<a href='#' class='project-popover' data-toggle='popover' data-content='" +
-        desc + "' data-html='true' data-original-title='" +
-        tmp + "' data-placement='right' data-viewport='#body'>" +
-        desc_short +
-        "</a>";
-
-    return desc_full;
 }
 /* ------------------------------------------------------------------------- */
 function displayCurrentFilters() {
